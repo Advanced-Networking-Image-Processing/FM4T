9df6cfd8305957c984fced9ca66cc49d9a1abf5e
Srutarshi <SrutarshiBanerjee2022@u.northwestern.edu>
2025-12-05 01:37:52 -0600

    Update sample_condition_super_res_2x.py

--- FM_CT_git/sample_condition_super_res_2x.py
+++ FM_CT_git/sample_condition_super_res_2x.py
@@ -1,5 +1,5 @@
 from ldm_inverse.condition_methods import get_conditioning_method
-from ldm.models.diffusion.ddim_admm_match_sf1_diff_phy_guid.ddim_git_admm_match_sf1_p1e_4_pg20_inv_prb_not_matched import DDIMSampler
+from ldm.models.diffusion.ddim_git_admm_match_sf1_p1e_4_pg20_inv_prb_not_matched import DDIMSampler
 from data.dataloader import get_dataset, get_dataloader
 from scripts.utils import clear_color, mask_generator
 import matplotlib.pyplot as plt
@@ -28,7 +28,7 @@ def get_model(args):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_config', type=str)
-parser.add_argument('--ldm_config', default="/homes/sruban/latent-diffusion-inpainting_sino_trial/ldm/models/ldm/inpainting_big/config_rl_no_mask.yaml", type=str)
+parser.add_argument('--ldm_config', default="configs/latent-diffusion/config_rl_no_mask.yaml", type=str)
 parser.add_argument('--diffusion_config', default="/homes/sruban/latent-diffusion-inpainting_sino_trial/ldm_trained_AE_org_phy12_w_gan_Jan15_25/real_only/act_epchs_no_mask1/all_epchs/epoch=000205.ckpt", type=str)
 parser.add_argument('--task_config', default="configs/tasks/super_resolution_config_512_2x.yaml", type=str)
 parser.add_argument('--gpu', type=int, default=0)
